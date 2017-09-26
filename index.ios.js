62f1cdb8de722576d3f59cc8c9a98bcbd6b99d2e
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-09-26 18:43:07 +0300

    Add minimal repro of the bug

--- index.ios.js
+++ index.ios.js
@@ -3,13 +3,20 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
-import Mapbox, { MapView } from '@mapbox/react-native-mapbox-gl';
+import Mapbox, { MapView, Annotation } from '@mapbox/react-native-mapbox-gl';
 
 Mapbox.setAccessToken('pk.eyJ1IjoiaGVucmlrcmEiLCJhIjoiY2o1a3czMjA1MDlzejJwbXhpam1oMTJpMSJ9.TANJvIveftY7gEV8Um3Aew');
 
 export default class mapboxCustomAnnotations extends Component {
+  state = {showNewAnnotation: false}
+
+  toggleNewMarker = () => {
+    this.setState({showNewAnnotation: !this.state.showNewAnnotation})
+  }
+  
   render() {
     return (
       <View style={styles.container}>
@@ -17,7 +24,29 @@ export default class mapboxCustomAnnotations extends Component {
           style={styles.map} 
           initialCenterCoordinate={{ latitude: 65.0121, longitude: 25.4651 }} 
           initialZoomLevel={12}
-        />
+        >
+          <Annotation
+            id="fromMarker"
+            coordinate={{
+              latitude:  65.0121,
+              longitude: 25.4681,
+            }}
+          >
+            <View style={styles.customMarker} />
+          </Annotation>
+          {this.state.showNewAnnotation && (
+            <Annotation
+              id="toMarker"
+              coordinate={{
+                latitude: 65.0221,
+                longitude: 25.4751,
+              }}
+            >
+              <View style={styles.customMarker} />
+            </Annotation>
+          )}
+        </MapView>
+        <Button title="Show new marker" onPress={this.toggleNewMarker} />
       </View>
     );
   }
@@ -30,6 +59,11 @@ const styles = StyleSheet.create({
   map: {
     flex: 1
   },
+  customMarker: {
+    width: 75,
+    height: 75,
+    backgroundColor: 'red'
+  }
 });
 
 AppRegistry.registerComponent('mapboxCustomAnnotations', () => mapboxCustomAnnotations);
